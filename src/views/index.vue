ce7ddfa19d0159c858c95c6447d2a7e80ca04905
Evriyana Indra Saputra <34746470+evriyanaindrasaputra@users.noreply.github.com>
2022-11-01 14:33:31 +0700

    Initial commit

---
+++ src/views/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import HelloWorld from '~/components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold text-red-400 underline">
+      Hello world!
+    </h1>
+    <HelloWorld msg="Hallo" />
+  </div>
+</template>
+
