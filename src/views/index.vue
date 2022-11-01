7fa9d24607c1530e9db3f04fab46745f364cf505
Evriyana Indra Saputra <eindrasap@gmail.com>
2022-11-01 21:32:00 +0700

    fix: hot reload bug

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
+import AddIcon from '../components/Icons/AddIcon.vue'
 import Button from '~/components/Button/Button.vue'
-import AddIcon from '~/components/Icons/AddIcon.vue'
 </script>
 
 <template>
   <div>
     <h1 class="text-3xl font-bold text-green-200 underline font-Lato">
-      Hello world! wqw
+      Hello world! wqw asds ssss
     </h1>
     <Button icon>
       <AddIcon class="w-5 h-5" fill="fill-red-500" />
