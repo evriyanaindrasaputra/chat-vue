721a3a481d82f4572fa0bbeb667c895af1a8cb3e
Evriyana Indra Saputra <eindrasap@gmail.com>
2022-11-01 16:06:20 +0700

    feat: font lato

--- src/views/index.vue
+++ src/views/index.vue
@@ -4,7 +4,7 @@ import HelloWorld from '~/components/HelloWorld.vue'
 
 <template>
   <div>
-    <h1 class="text-3xl font-bold text-red-400 underline">
+    <h1 class="text-3xl font-bold text-red-400 underline font-Lato">
       Hello world!
     </h1>
     <HelloWorld msg="Hallo" />
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        Lato: ['Lato', 'sans-serif'],
+      },
       colors: {
         'primary-one': '#2F80ED',
         'primary-two': '#4F4F4F',
