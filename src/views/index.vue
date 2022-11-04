7389799ecab9dab0f4e455fe4a9fad58a038bc62
Evriyana Indra Saputra <eindrasap@gmail.com>
2022-11-04 11:33:15 +0700

    feat: chat

--- src/views/index.vue
+++ src/views/index.vue
@@ -70,7 +70,85 @@ const closeDiscuss = () => {
       </template>
       <ContainerDiscuss>
         <template v-if="discuss.isDiscuss">
-          <div class=" space-y-2">
+          <div class="space-y-2">
+            <div class="w-full  ml-auto">
+              <p class="text-chats-two-darken text-right">
+                You
+              </p>
+              <div class="flex items-start flex-row-reverse gap-1">
+                <div class="bg-chats-two p-4 rounded">
+                  <p class=" text-gray-700 text-sm">
+                    No worries. It will be completed ASAP. I’ve asked him yesterday
+                  </p>
+                  <span class="text-gray-700 text-xs">19:32</span>
+                </div>
+                <button>
+                  <MoreHorizIcon class="w-4 h-4 " fill="fill-black" />
+                </button>
+              </div>
+            </div>
+            <div class="w-full flex justify-between space-x-3 items-center">
+              <div class="w-full h-[2px] rounded bg-black" />
+              <div class=" flex-shrink-0 text-sm">
+                Today June 09, 2021
+              </div>
+              <div class="w-full h-[2px] rounded bg-black" />
+            </div>
+            <div class="w-full">
+              <span class="text-chats-two-darken">You</span>
+              <div class="flex items-start gap-1">
+                <div class="bg-chats-two p-4 rounded">
+                  <p class=" text-gray-700 text-sm">
+                    No worries. It will be completed ASAP. I’ve asked him yesterday
+                  </p>
+                  <span class="text-gray-700 text-xs">19:32</span>
+                </div>
+                <button>
+                  <MoreHorizIcon class="w-4 h-4 " fill="fill-black" />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="space-y-2">
+            <div class="w-full  ml-auto">
+              <p class="text-chats-two-darken text-right">
+                You
+              </p>
+              <div class="flex items-start flex-row-reverse gap-1">
+                <div class="bg-chats-two p-4 rounded">
+                  <p class=" text-gray-700 text-sm">
+                    No worries. It will be completed ASAP. I’ve asked him yesterday
+                  </p>
+                  <span class="text-gray-700 text-xs">19:32</span>
+                </div>
+                <button>
+                  <MoreHorizIcon class="w-4 h-4 " fill="fill-black" />
+                </button>
+              </div>
+            </div>
+            <div class="w-full flex justify-between space-x-3 items-center">
+              <div class="w-full h-[2px] rounded bg-black" />
+              <div class=" flex-shrink-0 text-sm">
+                Today June 09, 2021
+              </div>
+              <div class="w-full h-[2px] rounded bg-black" />
+            </div>
+            <div class="w-full">
+              <span class="text-chats-two-darken">You</span>
+              <div class="flex items-start gap-1">
+                <div class="bg-chats-two p-4 rounded">
+                  <p class=" text-gray-700 text-sm">
+                    No worries. It will be completed ASAP. I’ve asked him yesterday
+                  </p>
+                  <span class="text-gray-700 text-xs">19:32</span>
+                </div>
+                <button>
+                  <MoreHorizIcon class="w-4 h-4 " fill="fill-black" />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="space-y-2">
             <div class="w-full  ml-auto">
               <p class="text-chats-two-darken text-right">
                 You
